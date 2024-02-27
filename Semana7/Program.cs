01d5c050e55c6d0366a01c7262c18ef540bd79e6
juanluisgaros <157499138+jlgarciaos@users.noreply.github.com>
2024-02-27 09:31:36 -0600

    Add files via upload

---
+++ Semana7/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo proyecto");
+            string sNombre;
+            string sEdad;
+            string sCarrera;
+            string sCarnet;
+
+            Console.WriteLine("Ingrese su nombre");
+            sNombre=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad");
+            sEdad=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carnet");
+            sCarnet=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carrera");
+            sCarrera=Console.ReadLine();
+
+            Console.WriteLine("Soy " + sNombre + " tengo " + sEdad + " y estudio la carrera de " + sCarrera + " Mi numero de carnet es " + sCarnet);
+        }
+}
